2e00e1b7a9c7c3d01be415d190e4d0456322ab7a
harryjjun <wnsgh6315@gmail.com>
2019-12-06 16:55:47 +0900

    set model and partition

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Board, Article
 
 # Register your models here.
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('pk',)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'content', 'board_id',)
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import architect
 
 # Create your models here.
+class Board(models.Model):
+    pass
+
+    def __str__(self):
+        return str(self.pk)
+
+@architect.install('partition', type='range', subtype='integer', constraint='2', column='board_id')
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
